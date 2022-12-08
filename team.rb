de13ccd96e283d9391cf4a3e3d139f83c344e8c8
tadafumioka <tadafumi_app@gmail.com>
2022-12-08 10:36:56 +0000

    kadai-ruby-oop-1

--- team.rb
+++ team.rb
@@ -3,7 +3,7 @@ class Team
  # オブジェクトの変数
  attr_accessor(:name, :win, :lose, :draw)
  
- def initialize(win, lose, draw)
+ def initialize(name, win, lose, draw)
   name = name
   self.win = win
   self.lose = lose
@@ -18,19 +18,19 @@ class Team
 # インスタンスが持つメソッド（チームの成績をターミナルに表示する）
 # ○○ の2020年の成績は △△勝 □□敗 ☆☆分、勝率は 0.▽▽▽▽▽▽です。
  def show_team_result(name)
-    puts "#{name} の2020年の成績は #{self.win} 勝 #{self.lose}敗 #{self.draw}分、勝率は #{win.to_f/(win + lose) } です。"
+    puts "#{name} の2020年の成績は #{self.win} 勝 #{self.lose}敗 #{self.draw}分、勝率は #{calc_win_rate(name)} です。"
  end
  
 end
 
 
 # インスタンスの生成と、変数への代入
-team_A = Team.new(67, 45, 8)
-team_B = Team.new(60, 53, 7)
-team_C = Team.new(60, 55, 5)
-team_D = Team.new(56, 58, 6)
-team_E = Team.new(52, 56, 12)
-team_F = Team.new(41, 69, 10)
+team_A = Team.new('Giants', 67, 45, 8)
+team_B = Team.new('Tigers', 60, 53, 7)
+team_C = Team.new('Dragons', 60, 55, 5)
+team_D = Team.new('BayStars', 56, 58, 6)
+team_E = Team.new('Carp', 52, 56, 12)
+team_F = Team.new('Swallows', 41, 69, 10)
 
 # インスタンスの使用
 team_A.show_team_result('Giants')
@@ -39,10 +39,3 @@ team_C.show_team_result('Dragons')
 team_D.show_team_result('BayStars')
 team_E.show_team_result('Carp')
 team_F.show_team_result('Swallows')
-
-p team_A
-p team_B
-p team_C
-p team_D
-p team_E
-p team_F
