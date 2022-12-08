5282eb242c384ce149a7f392977f6e172f2d762f
tadafumioka <tadafumi_app@gmail.com>
2022-12-08 11:41:19 +0000

    kadai-ruby-oop-1

--- team.rb
+++ team.rb
@@ -4,21 +4,21 @@ class Team
  attr_accessor(:name, :win, :lose, :draw)
  
  def initialize(name, win, lose, draw)
-  name = name
+  self.name = name
   self.win = win
   self.lose = lose
   self.draw = draw
  end
 
 # インスタンスが持つメソッド（勝率）
- def calc_win_rate(name)
+ def calc_win_rate
   win.to_f/(win + lose)  
  end
  
 # インスタンスが持つメソッド（チームの成績をターミナルに表示する）
 # ○○ の2020年の成績は △△勝 □□敗 ☆☆分、勝率は 0.▽▽▽▽▽▽です。
- def show_team_result(name)
-    puts "#{name} の2020年の成績は #{self.win} 勝 #{self.lose}敗 #{self.draw}分、勝率は #{calc_win_rate(name)} です。"
+ def show_team_result
+  puts "#{self.name} の2020年の成績は #{self.win} 勝 #{self.lose}敗 #{self.draw}分、勝率は #{calc_win_rate} です。"
  end
  
 end
@@ -33,9 +33,9 @@ team_E = Team.new('Carp', 52, 56, 12)
 team_F = Team.new('Swallows', 41, 69, 10)
 
 # インスタンスの使用
-team_A.show_team_result('Giants')
-team_B.show_team_result('Tigers')
-team_C.show_team_result('Dragons')
-team_D.show_team_result('BayStars')
-team_E.show_team_result('Carp')
-team_F.show_team_result('Swallows')
+team_A.show_team_result
+team_B.show_team_result
+team_C.show_team_result
+team_D.show_team_result
+team_E.show_team_result
+team_F.show_team_result
